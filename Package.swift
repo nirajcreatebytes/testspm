26710d49fe2558db31bf754e5201f222634bb50c
niraj <niraj@createbytes.com>
2023-04-20 13:28:54 +0530

    new version added
    new version added

--- Package.swift
+++ Package.swift
@@ -26,5 +26,5 @@ let package = Package(
             name: "testspmTests",
             dependencies: ["testspm"]),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.version("5.7")]
 )
